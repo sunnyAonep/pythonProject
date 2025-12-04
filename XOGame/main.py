c21196c6f910cacb966ff661101aea8071fe5543
sunnyAonep <arbiv.sunny@gmail.com>
2025-12-04 13:45:03 +0200

    v-0.1

--- XOGame/main.py
+++ XOGame/main.py
@@ -2,28 +2,31 @@ table = {1: [1,2,3], 2: [4,5,6], 3: [7,8,9]}
 finish = False
 
 def check_row(table):
-    for r in range(1, 4): 
-            if table[r][0] == table[r][1] == table[r][2] != " ":
-                print(f"Player {player} wins!")
-                finish = True
-                break
+    for r in range(1, 4):
+        if table[r][0] == table[r][1] == table[r][2] != " ":
+            return True
+    return False
+
 
 def check_col(table):
-    if not finish:
-            for c in range(3):  
-                if table[1][c] == table[2][c] == table[3][c] != " ":
-                    print(f"Player {player} wins!")
-                    finish = True
-                    break
+    for c in range(3):
+        if table[1][c] == table[2][c] == table[3][c] != " ":
+            return True
+    return False
+
 
 def check_dig(table):
-    if not finish:
-            if table[1][0] == table[2][1] == table[3][2] != " ":
-                print(f"Player {player} wins!")
-                finish = True
-            elif table[1][2] == table[2][1] == table[3][0] != " ":
-                print(f"Player {player} wins!")
-                finish = True
+    if table[1][0] == table[2][1] == table[3][2] != " ":
+        return True
+    if table[1][2] == table[2][1] == table[3][0] != " ":
+        return True
+    return False
+
+def check_winner(table, player):
+    if check_row(table) or check_col(table) or check_dig(table):
+        print(f"Player {player} wins!")
+        return True
+    return False
 
 def identify_X_or_O(xo):
    if(xo=='X' or xo=='O'):
@@ -49,9 +52,12 @@ def is_valid_move(table, row, col):
 def make_move(table, row, col, player):
     table[row][col] = player
 
+#main game loop:
+
 while finish == False:
     player1_symbol = 'X'
     player2_symbol = 'O'
+
     for i in range(9):
         print(f"{table[1]}\n{table[2]}\n{table[3]}")
         player_choice = int(input("First player, choose your block (1-9): "))
@@ -61,7 +67,10 @@ while finish == False:
             table[2][player_choice-4] = player1_symbol
         else:
             table[3][player_choice-7] = player1_symbol
-        check_col(table), check_row(table), check_dig(table)
+        if check_winner(table, player1_symbol):
+            finish = True
+            break
 
-    print("Draw")
-    finish = True
+
+    if finish == False: 
+        print("Draw")
