3b769f5a429a59235dda0925076ebbf6e4354976
lavi peer <lavipeerr@gmail.com>
2025-12-04 11:54:34 +0200

    update

--- XOGame/lavi.py
+++ XOGame/lavi.py
@@ -1,9 +1,7 @@
 # לוח המשחק
-row1 = [1,2,3]
-row2 = [4,5,6]
-row3 = [7,8,9]
-result = False
+table = {1: [1,2,3], 2: [4,5,6], 3: [7,8,9]}
+finish = False
 
-while result == True:
-
-
+while finish == False:
+    print(f"{table[1]}\n{table[2]}\n{table[3]}")
+    exit()
